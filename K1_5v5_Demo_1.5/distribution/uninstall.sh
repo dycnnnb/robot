ca2e03c9a98d3c4cceb9ab9cb10b067140770982
Unknownuserfrommars <163658509+Unknownuserfrommars@users.noreply.github.com>
2026-06-10 14:29:21 +0800

    Delete K1_5v5_Demo_1.5 directory

--- K1_5v5_Demo_1.5/distribution/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/home/booster/Workspace/robocup/scripts/stop.sh
-rm -rf /home/booster/Workspace/robocup
-systemctl --user stop robocup_game_assist.service
-systemctl --user disable robocup_game_assist.service
-systemctl --user daemon-reload
-rm /home/booster/.config/systemd/user/robocup_game_assist.service
--- K1_5v5_Demo_1.5/scripts/build_brain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`
-cd ..
-
-colcon build  --symlink-install  --parallel-workers $(nproc) --packages-select brain "$@"
-espeak "build complete" >/dev/null 2>&1 || echo "Build complete"
